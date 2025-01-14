1c4bb0ac79bd43aed4781c0988e1753eae6a9080
kovendhan-b <kofilebackup@gmail.com>
2025-01-14 20:34:25 +0530

    Base Frontend Completed

--- app.py
+++ app.py
@@ -9,10 +9,9 @@ vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('Home.html')
 
 @app.route('/predict_api',methods=['POST'])
-
 def predict_api():
     data=request.json['data']
     print(data)
@@ -20,6 +19,17 @@ def predict_api():
     output=model.predict(vect_data)
     print(output[0])
     return jsonify(int(output[0]))
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    text = [request.form['text']]
+    print(f"Received input: {text}")
+    vect_data = vectorizer.transform(text)
+    print(f"Vectorized input: {vect_data}")
+    output = model.predict(vect_data)
+    print(f"Model output: {output}")
+    sentiment = "Negative" if output[0] == -1 else "Neutral" if output[0] == 0 else "Positive"
+    return render_template("Home.html", prediction_text="Sentiment of text is {}".format(sentiment))
 
 if __name__=="__main__":
     app.run(debug=True)
